8669c6fb08b8aea8d69a6f829a6ceb04d1b1f16e
Jisoo Yoo <youzysu@gmail.com>
2023-04-04 17:06:36 +0900

    feat: 컴포넌트 단위 개발을 위한 Element 클래스 생성
    - tagName을 인자로 받아 해당 태그의 노드를 생성한다.

---
+++ src/js/utils/querySelector.js
@@ -0,0 +1,11 @@
+export const $ = (selector, context = document) => {
+  const $element = context.querySelector(selector);
+  if ($element === null) {
+    throw new Error(`선택자 ${selector}에 해당되는 요소를 찾을 수 없습니다`);
+  }
+  return $element;
+};
+
+export const $context = (context = document) => {
+  return (selector) => $(selector, context);
+};
