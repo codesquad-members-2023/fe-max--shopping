c4025d1d9e4df4f4b028e726dd47fa6b407f0985
jay <chlwodnjs34@gmail.com>
2023-04-05 10:56:31 +0900

    refact: 가독성을 위해  줄바꿈

--- src/js/component/header/navBar/BI.js
+++ src/js/component/header/navBar/BI.js
@@ -4,10 +4,13 @@ export class BI extends Base {
   constructor() {
     super("h1");
     this.setAttribute("id", "BI");
-    const aNode = new Base("a");
+
     const BiImg = new Base("img");
     BiImg.setAttribute("src", "./src/assets/BI.svg");
+
+    const aNode = new Base("a");
     aNode.setChildren(BiImg);
+
     this.setChildren(aNode);
   }
 }
