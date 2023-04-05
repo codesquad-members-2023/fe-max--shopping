4e897e6ed6b78c38627fa8ffd7b330af56b41b54
Jisoo Yoo <youzysu@gmail.com>
2023-04-06 02:12:28 +0900

    feat: [로그인 영역] 마우스 호버 시 확장 레이어 렌더링, 배경 딤처리하기

--- src/js/components/Navbar/NavbarMain/Login.js
+++ src/js/components/Navbar/NavbarMain/Login.js
@@ -1,11 +1,36 @@
-import { LoginModal } from '../../element/Modal.js';
+import { DimLayer } from '../../element/DimLayer.js';
+import { ExtendedLoginModal, LoginModal } from '../../element/Modal.js';
 import { Component } from '/src/js/components/base/Component.js';
 
 export class Login extends Component {
   constructor() {
     super('login');
     this.loginModalNode = new LoginModal().node;
-    this.node.append(this.loginModalNode);
+    this.extendedLoginModalNode = new ExtendedLoginModal().node;
+    this.dimLayer = new DimLayer();
+    this.node.append(this.loginModalNode, this.extendedLoginModalNode);
+  }
+
+  initEventHandlers() {
+    setTimeout(() => this.showLoginModal(), 1000);
+    this.node.addEventListener('mouseenter', () => this.showExtendedLoginModal());
+    this.node.addEventListener('mouseleave', () => this.closeExtendedLoginModal());
+  }
+
+  showExtendedLoginModal() {
+    this.loginModalNode.close();
+    this.extendedLoginModalNode.show();
+    this.dimLayer.on();
+  }
+
+  closeExtendedLoginModal() {
+    this.loginModalNode.close();
+    this.extendedLoginModalNode.close();
+    this.dimLayer.off();
+  }
+
+  showLoginModal() {
+    this.loginModalNode.show();
   }
 
   template() {
@@ -13,13 +38,5 @@ export class Login extends Component {
 <span class="label-text">안녕하세요, 로그인</span>
 <span class="main-text">계정 및 목록</span>
     `;
-  }
-
-  initEventHandlers() {
-    setTimeout(() => this.showLoginModal(), 1000);
-  }
-
-  showLoginModal() {
-    this.loginModalNode.show();
   }
 }
--- src/js/components/element/Modal.js
+++ src/js/components/element/Modal.js
@@ -16,6 +16,50 @@ export class LoginModal extends Component {
   }
 }
 
+export class ExtendedLoginModal extends Component {
+  constructor() {
+    super('modal extended-login-modal', 'DIALOG');
+  }
+
+  template() {
+    return `
+<div class="base">
+  <button class="login-btn">로그인</button>
+  <div>
+    <span>기존 사용자가 아니십니까?</span>
+    <span><a href="/">여기에서 시작합니다.</a></span>
+  </div>
+</div>
+<div class="extra">
+  <div>
+    <h3>귀하의 목록</h3>
+    <ul>
+      <li>목록 생성</li>
+      <li>목록 또는 레지스트리 찾기</li>
+      <li>AmazonSmile 자선 품목 목록</li>
+    </ul>
+  </div>
+  <div>
+    <h3>계정</h3>
+    <ul>
+      <li>계정</li>
+      <li>주문</li>
+      <li>권장 사항</li>
+      <li>검색 기록</li>
+      <li>워치리스트</li>
+      <li>비디오 구매 및 대여</li>
+      <li>Kindle 언리미티드</li>
+      <li>콘텐츠 및 기기</li>
+      <li>항목 구독 및 저장</li>
+      <li>멤버십 및 구독</li>
+      <li>음악 라이브러리</li>
+    </ul>
+  </div>
+</div>
+    `;
+  }
+}
+
 export class ShippingModal extends Component {
   constructor() {
     super('modal shipping-modal', 'DIALOG');
