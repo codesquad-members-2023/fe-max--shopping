a49f572d7e94dacbadd1970c41adf7017c96c1fd
Daeram Chung <daeram.chung@gmail.com>
2023-04-06 02:02:28 +0900

    Add card images and corresponding data structure

---
+++ src/data/index.js
@@ -0,0 +1,80 @@
+export const cards = [
+  {
+    imgSrc: "src/assets/images/card1.png",
+    imgAlt: "Card 1",
+    title: "집에서 입어보기",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card2.png",
+    imgAlt: "Card 2",
+    title: "특가",
+    link: "지금 쇼핑하세요",
+  },
+  {
+    imgSrc: "src/assets/images/card3.png",
+    imgAlt: "Card 3",
+    title: "피트니스용",
+    link: "지금 쇼핑하세요",
+  },
+  {
+    imgSrc: "src/assets/images/card4.png",
+    imgAlt: "Card 4",
+    title: "Amazon Basics",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card5.png",
+    imgAlt: "Card 5",
+    title: "건강 및 퍼스널 케어",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card6.png",
+    imgAlt: "Card 6",
+    title: "가정 및 주방",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card7.png",
+    imgAlt: "Card 7",
+    title: "Click here to shop in English",
+    link: "Change your language preference",
+  },
+  {
+    imgSrc: "src/assets/images/card8.png",
+    imgAlt: "Card 8",
+    title: "드레스",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card9.png",
+    imgAlt: "Card 9",
+    title: "활동 추적기 및 스마트워치 쇼핑",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card10.png",
+    imgAlt: "Card 10",
+    title: "노트북 및 태블릿 쇼핑",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card11.png",
+    imgAlt: "Card 11",
+    title: "전자기기",
+    link: "더보기",
+  },
+  {
+    imgSrc: "src/assets/images/card12.png",
+    imgAlt: "Card 12",
+    title: "Kindle E-readers",
+    link: "지금 쇼핑하세요",
+  },
+  {
+    imgSrc: "src/assets/images/card13.png",
+    imgAlt: "Card 13",
+    title: "반려동물용품 쇼핑",
+    link: "더보기",
+  },
+];
