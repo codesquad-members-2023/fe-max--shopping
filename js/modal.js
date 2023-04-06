272b018114a08e510282675599f29973f7c6aa56
silvertae <massenger7@gmail.com>
2023-04-07 04:50:36 +0900

    feat: 로그인 영역 호버시 확장된 로그인 레이어 구현
    로그인 영역에 마우스를 호버했을때 확장된 로그인 레이어가 나타나면서
    배경이 딤 처리되는 기능을 구현했다.
    
    로그인영역이나 레이어영역을 벗어나면 사라지면서 딤처리가 풀려야하는데
    이 부분을 완전히 구현하지 못하였다. 둘 다 mouseleave 이벤트를 걸면
    로그인 영역을 벗어나자마자 로그인 레이어가 사라져버린다.

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,39 @@
+const navLogin = document.querySelector('#nav-login');
+const loginTextArea = document.querySelector('#nav-login-wrapper');
 const popover = document.querySelector('#nav-login-popover');
-const closeBtn = document.querySelector('.close');
+const expandedPopover = document.querySelector('#nav-login-popover-expanded');
+
+const backdrop = document.createElement('div');
+backdrop.classList.add('modal-backdrop');
+navLogin.parentNode.appendChild(backdrop);
 
 window.addEventListener('DOMContentLoaded', () => {
-	setTimeout(() => {
-		popover.show();
-		popover.classList.toggle('show');
-	}, 1000);
+  setTimeout(() => {
+    popover.show();
+    popover.classList.add('show');
+  }, 1000);
 });
 
-closeBtn.addEventListener('click', () => {
-	popover.close();
-	popover.classList.toggle('show');
+loginTextArea.addEventListener('mouseenter', () => {
+  popover.close();
+  popover.classList.remove('show');
+
+  expandedPopover.show();
+  expandedPopover.classList.add('show');
+  backdrop.style.display = 'block';
+  backdrop.classList.add('show');
+});
+
+// navLogin.addEventListener('mouseout', () => {
+//   expandedPopover.close();
+//   expandedPopover.classList.remove('show');
+//   backdrop.style.display = 'none';
+//   backdrop.classList.remove('show');
+// });
+
+expandedPopover.addEventListener('mouseleave', () => {
+  expandedPopover.close();
+  expandedPopover.classList.remove('show');
+  backdrop.style.display = 'none';
+  backdrop.classList.remove('show');
 });
