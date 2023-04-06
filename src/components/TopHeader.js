d160e1f6ea69a5906eed1bddd9541e2d3a15f7d2
Daeram Chung <daeram.chung@gmail.com>
2023-04-07 00:09:19 +0900

    Add login tooltip
    - Close login tooltip upon hover over login section of navbar.
    - Wrap navbar `a` tags with `div` to better accommodate tooltips.

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -5,13 +5,15 @@ template.innerHTML = `
       <a href="/" id="logo-container">
         <img src="src/assets/logo.png" alt="Amazon Logo" /></a>
       </a>
-      <a href="#" id="shipping-container">
-        <div>
-          <img src="src/assets/icons/location.svg" alt="Shipping" />
-          <span>배송처</span>
-        </div>
-        <span>대한민국</span>
-      </a>
+      <div class="nav-main__item-container">
+        <a href="#" id="shipping-link">
+          <div>
+            <img src="src/assets/icons/location.svg" alt="Shipping" />
+            <span>배송처</span>
+          </div>
+          <span>대한민국</span>
+        </a>
+      </div>
       <div id="search-container">
         <form>
           <input type="text" placeholder="검색 Amazon" />
@@ -20,22 +22,37 @@ template.innerHTML = `
           </button>
         </form>
       </div>
-      <a href="#" id="lang-container">
-        <span>🇰🇷</span>
-        <span>KO</span>
-      </a>
-      <a href="#" class="tool-tip-parent" id="login-container">
-        <span>안녕하세요, 로그인</span>
-        <span>계정 및 목록</span>
-      </a>
-      <a href="#" id="orders-container">
-        <span>반품</span>
-        <span>& 주문</span>
-      </a>
-      <a href="#" id="cart-container">
-        <img src="src/assets/icons/cart.svg" alt="Shopping Cart" />
-        <span>장바구니</span>
-      </a>
+      <div class="nav-main__item-container">
+        <a href="#" id="lang-link">
+          <span>🇰🇷</span>
+          <span>KO</span>
+        </a>
+      </div>
+      <div class="nav-main__item-container tool-tip-parent">
+        <a href="#" id="login-link">
+          <span>안녕하세요, 로그인</span>
+          <span>계정 및 목록</span>
+        </a>
+
+        <tool-tip>
+          <div slot="tool-tip-content">
+            <primary-button data-content="로그인" data-width="160px"></primary-button>
+            <div class="blah">기존사용자가 아니십니까? <a href="#">여기에서 시작합니다.</a></div>
+          </div>
+        </tool-tip>
+      </div>
+      <div class="nav-main__item-container">
+        <a href="#" id="orders-link">
+          <span>반품</span>
+          <span>& 주문</span>
+        </a>
+      </div>
+      <div class="nav-main__item-container">
+        <a href="#" id="cart-link">
+          <img src="src/assets/icons/cart.svg" alt="Shopping Cart" />
+          <span>장바구니</span>
+        </a>
+      </div>
     </nav>
 
     <nav class="nav-sub">
@@ -64,6 +81,17 @@ class TopHeader extends HTMLElement {
     super();
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.append(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    const toolTipParents = this.shadowRoot.querySelectorAll(".tool-tip-parent");
+    toolTipParents.forEach((parent) => {
+      parent.addEventListener("mouseover", (evt) => {
+        const el = evt.target;
+        const toolTip = parent.querySelector("tool-tip");
+        if (!toolTip.contains(el)) toolTip.hideSelf();
+      });
+    });
   }
 }
 
