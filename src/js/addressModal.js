b4b47fccab130c138df167754d8ac0a23bef12f5
hjsong <song6240@naver.com>
2023-04-07 03:02:39 +0900

    feat: 주소 마우스 호버 핸들러 등록
    주소 영역 마우스 호버 시 동작
    - 주소 모달이 나타남.
    - 배경 딤처리.

---
+++ src/js/addressModal.js
@@ -0,0 +1,43 @@
+import { createButtonEl } from './button.js';
+import { addMouseoverHandler, createEmptyEl, showModalCover } from './loginModal.js';
+
+const addressTabEl = document.querySelector('.nav-bar__main-address');
+const addressModalEl = createAddressModal();
+
+export function initAddressModal() {
+  addressTabEl.append(addressModalEl);
+  addMouseoverHandler(addressTabEl, 'modal-address', showAddressModal);
+}
+
+function createAddressModal() {
+  const modalEl = createEmptyEl('modal-address');
+  const descriptionEl = createDescriptionEl();
+  const buttonsEl = createButtonsEl();
+  
+  modalEl.append(buttonsEl);
+  modalEl.insertAdjacentHTML('afterbegin', descriptionEl);
+
+  modalEl.style.display = 'none';
+
+  return modalEl;
+}
+
+function createDescriptionEl() {
+  return `<p>KR으로 배송할 품목을 표시하겠습니다. 다른 국가로 배송되는 품목을 보려면 배송 주소를 변경하십시오.</p>`
+}
+
+function createButtonsEl() {
+  const buttons = createEmptyEl("modal-address__buttons")
+  const cancelBtnEl = createButtonEl('계속');
+  const checkBtnEl = createButtonEl('주소 변경');
+
+  buttons.append(cancelBtnEl, checkBtnEl);
+
+  return buttons;
+}
+
+function showAddressModal(e) {
+  const addressModalEl = document.querySelector('.modal-address');
+  addressModalEl.style.display = 'flex';
+}
+
