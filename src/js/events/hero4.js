a8dcba9f70aeabe99462d5e385b9112e8b153ae5
JH <vvaa277@gmail.com>
2023-04-07 13:47:59 +0900

    Feat: 히어로 영역 구현중

---
+++ src/js/events/hero4.js
@@ -0,0 +1,104 @@
+import { $ } from '../utils/dom.js';
+
+let slideId;
+let index = 1;
+
+const getSlides = () => document.querySelectorAll('.slide-item');
+
+function initSlideShow() {
+  const slide = $('.slide-banner');
+  let slides = getSlides();
+
+  const firstClone = slides[0].cloneNode(true);
+  const lastClone = slides[slides.length - 1].cloneNode(true);
+
+  firstClone.id = 'first-clone';
+  lastClone.id = 'last-clone';
+
+  appendAndPrependClones({ slide, firstClone, lastClone });
+  setSlideInitialPosition({ slides, slide });
+  handleSlideTransitionEnd({ slide, firstClone, lastClone });
+}
+
+function setSlideInitialPosition({ slides, slide }) {
+  const slideWidth = slides[index].clientWidth;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+}
+
+function appendAndPrependClones({ slide, firstClone, lastClone }) {
+  slide.append(firstClone);
+  slide.prepend(lastClone);
+}
+
+function startSlide() {
+  slideId = setInterval(() => {
+    moveToNextSlide();
+  }, 3000);
+}
+
+function handleSlideTransitionEnd({ slide, firstClone, lastClone }) {
+  slide.addEventListener('transitionend', () => {
+    let slides = getSlides();
+    if (slides[index].id === firstClone.id) {
+      slide.style.transition = 'none';
+      // 트랜지션이 있으면 backward 이동하는게 보임
+      index = 1;
+      setSlideInitialPosition({ slides, slide });
+    }
+
+    if (slides[index].id === lastClone.id) {
+      slide.style.transition = 'none';
+      index = slides.length - 2;
+      setSlideInitialPosition({ slides, slide });
+    }
+  });
+}
+
+//
+const moveToNextSlide = () => {
+  const slide = $('.slide-banner');
+  let slides = getSlides();
+  if (index >= slides.length - 1) return;
+  index += 1;
+  slide.style.transition = '.5s ease-out';
+  setSlideInitialPosition({ slides, slide });
+};
+
+const moveToPreviousSlide = () => {
+  const slide = $('.slide-banner');
+  let slides = getSlides();
+  if (index <= 0) return;
+  index -= 1;
+  slide.style.transition = '.5s ease-out';
+  setSlideInitialPosition({ slides, slide });
+};
+
+function pauseOnHover() {
+  const slideContainer = $('.hero');
+  slideContainer.addEventListener('mouseenter', () => {
+    clearInterval(slideId);
+  });
+
+  slideContainer.addEventListener('mouseleave', startSlide);
+}
+
+function slideNavigation() {
+  const slideButtonLayout = $('.slide-buttons-layout');
+  slideButtonLayout.addEventListener('click', moveToNextOrPrevSlide);
+}
+
+function moveToNextOrPrevSlide(e) {
+  if (e.target.matches('.next')) {
+    moveToNextSlide();
+  }
+  if (e.target.matches('.prev')) {
+    moveToPreviousSlide();
+  }
+}
+
+function slideshowControls() {
+  pauseOnHover();
+  slideNavigation();
+}
+
+export { initSlideShow, startSlide, slideshowControls };
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,19 @@
-import { initLoginModal } from "../js/events/initLoginModal.js";
-import { initShippingModal } from "../js/events/initShippingModal.js";
-console.log(screen.width);
-console.log(document.documentElement.scrollWidth);
+import { initLoginModal } from '../js/events/initLoginModal.js';
+import { initShippingModal } from '../js/events/initShippingModal.js';
+// import { initHeroSlide, initLoop } from "../js/events/initHeroSlide.js";
+import { startSlide, slideshowControls, initSlideShow } from '../js/events/hero4.js';
+import { loadProducts } from '../js/events/getProducts.js';
+import { initContents } from '../js/events/initContents.js';
 
-
-
-const init = () =>{
-    initLoginModal()
-    initShippingModal()
-}
-init()
+const init = () => {
+  initLoginModal();
+  initShippingModal();
+  //   startSlide(); // hero3
+  startSlide(); // hero4
+  initSlideShow()
+  slideshowControls()
+  
+  loadProducts();
+  initContents();
+};
+init();
