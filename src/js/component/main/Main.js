3c3755bee4ce9b6bd669031a1c857bb49be09a45
jay <chlwodnjs34@gmail.com>
2023-04-07 17:58:40 +0900

    feat: 캐러셀 슬라이더 구현

--- src/js/component/main/Main.js
+++ src/js/component/main/Main.js
@@ -1,3 +1,15 @@
-export class Main {
-  constructor() {}
+import { Base } from "../Base.js";
+import { Carousel } from "./carousel/Carousel.js";
+
+export class Main extends Base {
+  constructor() {
+    super("div");
+    this.carousel = new Carousel();
+    this.init();
+  }
+
+  init() {
+    this.setAttribute("id", "main");
+    this.setChildren(this.carousel);
+  }
 }
