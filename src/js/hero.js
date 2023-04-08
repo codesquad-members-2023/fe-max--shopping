c1489974239e740f00d066d5479bfca1d6978c62
hjsong <song6240@naver.com>
2023-04-09 04:49:43 +0900

    style: 히어로 영역을 벗어난 이미지 숨김
    css를 통해서 overflow된 이미지를 감춤.
    브라우저 창 크기 조정할 때 이미지가 보이는 현상을 없애기 위해 현재
    이미지 외 항목들은 visibility: hidden 적용함.

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,36 +1,39 @@
-const slide = document.querySelector(".hero__slide");
-const images = document.querySelectorAll(".hero__slide img");
+const slide = document.querySelector('.hero__slide');
+const items = slide.children;
 
-const prevBtn = document.querySelector(".hero__prev-btn");
-const nextBtn = document.querySelector(".hero__next-btn");
+const prevBtn = document.querySelector('.hero__prev-btn');
+const nextBtn = document.querySelector('.hero__next-btn');
 
-const width = images[0].clientWidth;
-const middleIndex = Math.floor(images.length / 2);
+const width = items[0].clientWidth;
+const middleIndex = Math.floor(items.length / 2);
 let counterIndex = middleIndex;
 let isTransitioning = false;
 
+hideExceptIndex(items, counterIndex);
 slide.style.transform = `translateX(${-width * counterIndex}px)`;
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener('click', () => {
   if (isTransitioning) return;
 
   isTransitioning = true;
-  slide.style.transition = "transform 117ms";
+  slide.style.transition = 'all 117ms';
   counterIndex++;
+  items[counterIndex].style.visibility = `visible`;
   slide.style.transform = `translateX(${-width * counterIndex}px)`;
 });
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener('click', () => {
   if (isTransitioning) return;
 
   isTransitioning = true;
-  slide.style.transition = "transform 117ms";
+  slide.style.transition = 'all 117ms';
   counterIndex--;
+  items[counterIndex].style.visibility = `visible`;
   slide.style.transform = `translateX(${-width * counterIndex}px)`;
 });
 
-slide.addEventListener("transitionend", () => {
-  slide.style.transition = "none";
+slide.addEventListener('transitionend', () => {
+  slide.style.transition = 'none';
   if (counterIndex > middleIndex) {
     slide.append(slide.firstElementChild);
     counterIndex--;
@@ -38,6 +41,18 @@ slide.addEventListener("transitionend", () => {
     slide.prepend(slide.lastElementChild);
     counterIndex++;
   }
+
+  hideExceptIndex(items, counterIndex);
   slide.style.transform = `translateX(${-width * counterIndex}px)`;
   isTransitioning = false;
 });
+
+function hideExceptIndex(items, index) {
+  for (let i = 0; i < items.length; i++) {
+    if (i == index) {
+      items[i].style.visibility = 'visible';
+      continue;
+    }
+    items[i].style.visibility = 'hidden';
+  }
+}
