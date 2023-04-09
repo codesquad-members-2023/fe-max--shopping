c5e680e42b12f4d9aedcef47cb366e3fe2bd76a5
JH <vvaa277@gmail.com>
2023-04-09 14:39:39 +0900

    Refactor: 확장된 로그인 모달 템플릿 변수로 추출

--- src/js/events/initLoginModal.js
+++ src/js/events/initLoginModal.js
@@ -26,33 +26,32 @@ async function renderLoginModal() {
 function expandLoginModal() {
   const loginModal = $('.modal');
   const bodyLayout = $('.body-layout');
+  const expandedModalTemplate = `
+  <div class="line"></div>
+  <div class="modal-text-layout">
+      <dl>
+          <dt>귀하의 목록</dt>
+          <dd>목록 생성</dd>
+          <dd>목록 또는 레지스트리 찾기</dd>
+          <dd>AmazonSmile 자선 품목 목록</dd>
+      </dl>
+      <dl>
+          <dt>계정</dt>
+          <dd>계정</dd>
+          <dd>주문</dd>
+          <dd>권장 사항</dd>
+          <dd>검색 기록</dd>
+          <dd>워치리스트</dd>
+          <dd>비디오 구매 및 대여</dd>
+          <dd>Kindle 언리미티드</dd>
+          <dd>콘텐츠 및 기기</dd>
+          <dd>항목 구독 및 저장</dd>
+          <dd>멤버십 및 구독</dd>
+          <dd>음악 라이브러리</dd>
+      </dl>
+  </div>`;
 
-  loginModal.insertAdjacentHTML(
-    'beforeend',
-    `<div class="line"></div>
-      <div class="modal-text-layout">
-          <dl>
-              <dt>귀하의 목록</dt>
-              <dd>목록 생성</dd>
-              <dd>목록 또는 레지스트리 찾기</dd>
-              <dd>AmazonSmile 자선 품목 목록</dd>
-          </dl>
-          <dl>
-              <dt>계정</dt>
-              <dd>계정</dd>
-              <dd>주문</dd>
-              <dd>권장 사항</dd>
-              <dd>검색 기록</dd>
-              <dd>워치리스트</dd>
-              <dd>비디오 구매 및 대여</dd>
-              <dd>Kindle 언리미티드</dd>
-              <dd>콘텐츠 및 기기</dd>
-              <dd>항목 구독 및 저장</dd>
-              <dd>멤버십 및 구독</dd>
-              <dd>음악 라이브러리</dd>
-          </dl>
-      </div>`
-  );
+  loginModal.insertAdjacentHTML('beforeend', expandedModalTemplate);
 
   bodyLayout.insertAdjacentHTML(
     'beforeend',
