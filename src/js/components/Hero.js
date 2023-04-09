f30eecd46b5e161fec64d83ca4269b543fbf2887
Jisoo Yoo <youzysu@gmail.com>
2023-04-09 23:02:47 +0900

    refactor: 히어로 슬라이더 첫번째 이미지로 수정

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -8,7 +8,7 @@ export class Hero extends Component {
     super('hero');
     this.slideCount = slideCount;
     this.lastIndex = this.slideCount + 1;
-    this.currentIndex = this.slideCount;
+    this.currentIndex = 1;
     this.slider = this.$('.slider');
     this.initSlider(this.slider, this.slideCount);
   }
@@ -73,7 +73,7 @@ export class Hero extends Component {
     this.setSliderWidth(slider, slideCount);
     this.appendSlides(slider, slideCount);
     this.renderSlideClone(slider);
-    slider.style.transform = `translateX(-${slideCount * 100}vw)`;
+    slider.style.transform = `translateX(-100vw)`;
   }
 
   setSliderWidth(slider, slideCount) {
