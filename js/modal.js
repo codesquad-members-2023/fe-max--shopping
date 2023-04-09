8e8cc6c4b42e216d89a88b4af326eb9b17c24ab2
silvetae <massenger7@gmail.com>
2023-04-10 03:52:27 +0900

    refactor: 상단바 모달 관련 기능 전반적인 리팩터링
    기존에 dialog 태그를 사용하던 모달 방식은 여러 제약들이 있다고 생각돼서
    div로 새롭게 모달을 만들어서 구현하는 것으로 변경하였다.
    
    그리고 BEM 클래스 작명법에 대해서 공부해서 조금씩 적용해보는 중이다.
    
    상단 네비게이션 주소영역 모달창 기능도 추가하였다.

--- js/modal.js
+++ js/modal.js
@@ -4,37 +4,43 @@ const loginArea = document.querySelector('#nav-login');
 const loginTextArea = document.querySelector('#nav-login-text');
 const loginPopover = document.querySelector('#nav-login-popover');
 const loginModal = document.querySelector('#nav-login-modal');
-const backdrop = document.querySelector('.modal-backdrop');
+const addressArea = document.querySelector('#nav-shipping-address');
+const addressModal = document.querySelector('#nav-address-modal');
+const backdrop = document.querySelector('.modal__backdrop');
 
 export function initHeader() {
   setEvent(window, 'DOMContentLoaded', fadeInLoginPopover);
   setEvent(loginTextArea, 'mouseenter', showLoginModal);
   setEvent(loginArea, 'mouseleave', hideLoginModal);
+  setEvent(addressArea, 'mouseenter', showAddressModal);
+  setEvent(addressArea, 'mouseleave', hideAddressModal);
 }
 
 function fadeInLoginPopover() {
   setTimeout(() => {
-    loginPopover.show();
     loginPopover.classList.add('show');
   }, 1000);
 }
 
 function showLoginModal() {
-  if (loginPopover) {
-    loginPopover.close();
-    loginPopover.classList.remove('show');
-  }
-  loginModal.show();
-  loginModal.style.display = 'flex';
+  loginPopover.style.display = 'none';
+  loginPopover.classList.remove('show');
+
   loginModal.classList.add('show');
-  backdrop.style.display = 'block';
   backdrop.classList.add('show');
 }
 
 function hideLoginModal() {
-  loginModal.close();
-  loginModal.style.display = 'none';
   loginModal.classList.remove('show');
-  backdrop.style.display = 'none';
+  backdrop.classList.remove('show');
+}
+
+function showAddressModal() {
+  addressModal.classList.add('show');
+  backdrop.classList.add('show');
+}
+
+function hideAddressModal() {
+  addressModal.classList.remove('show');
   backdrop.classList.remove('show');
 }
