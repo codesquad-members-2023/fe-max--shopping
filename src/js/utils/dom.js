fe684c56647aec94cb3ca4daa8bd6f5b4ef8e367
JH <vvaa277@gmail.com>
2023-04-09 14:10:20 +0900

    Refactor: DOM 탐색 root도 인자로 받을 수 있도록 변경

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,4 +1,4 @@
-const $ = (selector) => document.querySelector(selector);
-const $All = (selector) => document.querySelectorAll(selector);
+const $ = (selector, root = document) => root.querySelector(selector);
+const $All = (selector, root = document) => root.querySelectorAll(selector);
 
 export { $, $All };
