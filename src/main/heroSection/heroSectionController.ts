865e289cd3a1195ef7645cdcf7209dc193e16bf7
jsh3418 <albert5428@gmail.com>
2023-04-10 11:53:49 +0900

    refactor: heroSectionController 동작 분리

--- src/main/heroSection/heroSectionController.ts
+++ src/main/heroSection/heroSectionController.ts
@@ -1,44 +1,26 @@
 import { $$ } from "../../utils/domUtils";
 
 export const handleImageMove = (() => {
-  const imageCount = $$(".hero-section__image").length;
   let index = 0;
+  const imageCount = $$(".hero-section__image").length;
+  const isNegativeImageIndex = () => index < 0;
+  const isExceededImageIndex = () => index > imageCount - 1;
 
   return async (direction: string, $imageContainer: HTMLElement) => {
     index += direction === "prev" ? -1 : 1;
 
-    if (index < 0) {
+    if (isNegativeImageIndex()) {
       index = imageCount - 2;
 
-      if ($imageContainer.firstElementChild == null) {
-        throw new Error(`The image container(${$imageContainer}) is empty`);
-      }
-
-      $imageContainer.style.transitionDuration = "";
-      $imageContainer.insertAdjacentElement("beforeend", $imageContainer.firstElementChild);
-      $imageContainer.style.transform = `translateX(-${imageCount - 1}00%)`;
-      await delay(1);
-
-      $imageContainer.style.transitionDuration = "500ms";
-      $imageContainer.style.transform = `translateX(-${index * 100}%)`;
+      handleNegativeIndex(index, imageCount, $imageContainer);
 
       return;
     }
 
-    if (index > imageCount - 1) {
+    if (isExceededImageIndex()) {
       index = 1;
 
-      if ($imageContainer.lastElementChild == null) {
-        throw new Error(`The image container(${$imageContainer}) is empty`);
-      }
-
-      $imageContainer.style.transitionDuration = "";
-      $imageContainer.insertAdjacentElement("afterbegin", $imageContainer.lastElementChild);
-      $imageContainer.style.transform = `translateX(0%)`;
-      await delay(1);
-
-      $imageContainer.style.transitionDuration = "500ms";
-      $imageContainer.style.transform = `translateX(-${index * 100}%)`;
+      handleExceededImageIndex(index, $imageContainer);
 
       return;
     }
@@ -49,3 +31,35 @@ export const handleImageMove = (() => {
 })();
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const handleNegativeIndex = async (
+  index: number,
+  imageCount: number,
+  $imageContainer: HTMLElement
+) => {
+  if ($imageContainer.firstElementChild == null) {
+    throw new Error(`The image container(${$imageContainer}) is empty`);
+  }
+
+  $imageContainer.style.transitionDuration = "";
+  $imageContainer.insertAdjacentElement("beforeend", $imageContainer.firstElementChild);
+  $imageContainer.style.transform = `translateX(-${imageCount - 1}00%)`;
+  await delay(1);
+
+  $imageContainer.style.transitionDuration = "500ms";
+  $imageContainer.style.transform = `translateX(-${index * 100}%)`;
+};
+
+const handleExceededImageIndex = async (index: number, $imageContainer: HTMLElement) => {
+  if ($imageContainer.lastElementChild == null) {
+    throw new Error(`The image container(${$imageContainer}) is empty`);
+  }
+
+  $imageContainer.style.transitionDuration = "";
+  $imageContainer.insertAdjacentElement("afterbegin", $imageContainer.lastElementChild);
+  $imageContainer.style.transform = `translateX(0%)`;
+  await delay(1);
+
+  $imageContainer.style.transitionDuration = "500ms";
+  $imageContainer.style.transform = `translateX(-${index * 100}%)`;
+};
