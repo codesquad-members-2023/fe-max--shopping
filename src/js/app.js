9316631253a0213f1fa0190569fdef59b1fc78da
qkdflrgs <qkdflrgs@gmail.com>
2023-04-11 21:19:20 +0900

    feat: 히어로 영역 기능 및 추천 검색어 기능 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 import modalController from './components/modal.js';
+import heroController from './components/hero.js';
+import searchBarController from './components/search-bar.js';
 
 const app = () => {
   modalController();
+  heroController();
+  searchBarController();
 };
 
 app();
