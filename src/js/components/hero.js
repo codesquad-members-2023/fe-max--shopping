9116b81ea3dcebc7b136104710c1a9be34848421
hjsong <song6240@naver.com>
2023-04-12 00:40:33 +0900

    refactor: 히어로 영역 재구현
    dataset을 활용하는 방식으로 히어로 동작 재구현함.
    DOM 직접 조작은 리플로우가 발생하기 때문에 성능상 유리하기 때문에
    재구현함.

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,28 +1,25 @@
 import { $, $All } from '../utils/domUtils.js';
 
 const MAX_INDEX = $All('.hero__item').length - 1;
+const AUTO_SLIDE_WAITTIME = 10000;
 let activeIndex = 0;
+let autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
 
 function initHero() {
-  $('.hero__btn--next').addEventListener('click', slideToNextItem);
-  $('.hero__btn--prev').addEventListener('click', slideToPrevItem);
+  $('.hero__btn--prev').addEventListener('click', handlePrevBtnClick);
+  $('.hero__btn--next').addEventListener('click', handleNextBtnClick);
 }
 
-function slideToNextItem() {
-  const nextIndex = activeIndex + 1 <= MAX_INDEX ? activeIndex + 1 : 0;
+function handlePrevBtnClick() {
+  clearInterval(autoSlideTimer);
+  slideToPrevItem();
+  autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
+}
 
-  const currentItem = $(`.hero__item[data-index="${activeIndex}"]`);
-  const nextItem = $(`.hero__item[data-index="${nextIndex}"]`);
-
-  currentItem.dataset.status = 'left';
-  nextItem.style.transition = 'none';
-  nextItem.dataset.status = 'right';
-
-  setTimeout(() => {
-    nextItem.style.transition = '';
-    nextItem.dataset.status = 'active';
-    activeIndex = nextIndex;
-  });
+function handleNextBtnClick() {
+  clearInterval(autoSlideTimer);
+  slideToNextItem();
+  autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
 }
 
 function slideToPrevItem() {
@@ -34,6 +31,23 @@ function slideToPrevItem() {
   currentItem.dataset.status = 'right';
   nextItem.style.transition = 'none';
   nextItem.dataset.status = 'left';
+
+  setTimeout(() => {
+    nextItem.style.transition = '';
+    nextItem.dataset.status = 'active';
+    activeIndex = nextIndex;
+  });
+}
+
+function slideToNextItem() {
+  const nextIndex = activeIndex + 1 <= MAX_INDEX ? activeIndex + 1 : 0;
+
+  const currentItem = $(`.hero__item[data-index="${activeIndex}"]`);
+  const nextItem = $(`.hero__item[data-index="${nextIndex}"]`);
+
+  currentItem.dataset.status = 'left';
+  nextItem.style.transition = 'none';
+  nextItem.dataset.status = 'right';
 
   setTimeout(() => {
     nextItem.style.transition = '';
