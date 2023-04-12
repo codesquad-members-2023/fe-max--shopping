05824c2ac511d59a02781435e9ec9b7702032710
hjsong <song6240@naver.com>
2023-04-12 18:55:39 +0900

    fix: 페어리뷰 피드백 반영
    - hero 영역 중복 제거.
    - prev 버튼을 누른 후, 2번째 슬라이드가 prev처럼 동작하는 현상 수정.
    - 다음 active item을 이동시키고 5ms 후에 slide 동작을 실행시킴.

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -2,58 +2,57 @@ import { $, $All } from '../utils/domUtils.js';
 
 const MAX_INDEX = $All('.hero__item').length - 1;
 const AUTO_SLIDE_WAITTIME = 10000;
-let activeIndex = 0;
-let autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
+const PREV_ITEM = true;
+const NEXT_ITEM = false;
 
-function initHero() {
+let activeIndex = 0;
+let autoSlideTimer = setInterval(() => slideTo(NEXT_ITEM), AUTO_SLIDE_WAITTIME);
+
+export function initHero() {
   $('.hero__btn--prev').addEventListener('click', handlePrevBtnClick);
   $('.hero__btn--next').addEventListener('click', handleNextBtnClick);
 }
 
 function handlePrevBtnClick() {
   clearInterval(autoSlideTimer);
-  slideToPrevItem();
-  autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
+  slideTo(PREV_ITEM);
+  autoSlideTimer = setInterval(() => slideTo(NEXT_ITEM), AUTO_SLIDE_WAITTIME);
 }
 
 function handleNextBtnClick() {
   clearInterval(autoSlideTimer);
-  slideToNextItem();
-  autoSlideTimer = setInterval(slideToNextItem, AUTO_SLIDE_WAITTIME);
+  slideTo(NEXT_ITEM);
+  autoSlideTimer = setInterval(() => slideTo(NEXT_ITEM), AUTO_SLIDE_WAITTIME);
 }
 
-function slideToPrevItem() {
-  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : MAX_INDEX;
+function slideTo(itemDirection) {
+  const nextIndex = getNextIndex(itemDirection);
+  const { currentItemStatus, nextItemStatus } = getItemsStatus(itemDirection);
 
   const currentItem = $(`.hero__item[data-index="${activeIndex}"]`);
   const nextItem = $(`.hero__item[data-index="${nextIndex}"]`);
 
-  currentItem.dataset.status = 'right';
   nextItem.style.transition = 'none';
-  nextItem.dataset.status = 'left';
-
+  nextItem.dataset.status = nextItemStatus;
+  
   setTimeout(() => {
     nextItem.style.transition = '';
+    currentItem.dataset.status = currentItemStatus;
     nextItem.dataset.status = 'active';
     activeIndex = nextIndex;
-  });
+  }, 5);
 }
 
-function slideToNextItem() {
-  const nextIndex = activeIndex + 1 <= MAX_INDEX ? activeIndex + 1 : 0;
-
-  const currentItem = $(`.hero__item[data-index="${activeIndex}"]`);
-  const nextItem = $(`.hero__item[data-index="${nextIndex}"]`);
-
-  currentItem.dataset.status = 'left';
-  nextItem.style.transition = 'none';
-  nextItem.dataset.status = 'right';
-
-  setTimeout(() => {
-    nextItem.style.transition = '';
-    nextItem.dataset.status = 'active';
-    activeIndex = nextIndex;
-  });
+function getNextIndex(isPrevItem) {
+  if (isPrevItem) {
+    return activeIndex - 1 >= 0 ? activeIndex - 1 : MAX_INDEX;
+  } 
+  return activeIndex + 1 <= MAX_INDEX ? activeIndex + 1 : 0;
 }
 
-export { initHero };
+function getItemsStatus(isPrevItem) {
+  return {
+    currentItemStatus: isPrevItem ? 'right' : 'left',
+    nextItemStatus: isPrevItem ? 'left' : 'right'
+  }
+}
