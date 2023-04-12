a9dc08d2ad772d6202e8df8e9dc1291b9417dd0a
JH <vvaa277@gmail.com>
2023-04-12 16:43:17 +0900

    Feat: 검색바 구현중...

--- src/js/events/initSearchBar.js
+++ src/js/events/initSearchBar.js
@@ -3,12 +3,17 @@ import { handleDimming, modalState } from '../utils/dim.js';
 const mainSearchBarArea = document.querySelector('.main-search-bar');
 const searchBar = document.searchForm.searchBar;
 const searchLayer = document.querySelector('.search-layer');
-const firstLi = searchLayer.firstElementChild;
-const lastLi = searchLayer.lastElementChild;
 const esc = 27;
 
 export function initSearchBar() {
-  mainSearchBarArea.addEventListener('click', handleModal);
+  mainSearchBarArea.addEventListener('click', () => {
+    handleModal();
+    const jsonData = new JsonData();
+    const data = jsonData.load('./src/data/db.json');
+    console.log(data);
+    const resultView = new ResultView(searchLayer);
+    resultView.renderSuggestion(data);
+  });
   document.addEventListener('click', removeModal);
   searchBar.addEventListener('keydown', keyboardNavigationHandler);
   searchLayer.addEventListener('keydown', addFocus);
@@ -28,24 +33,36 @@ function removeModal(e) {
   }
 }
 
+function getFirstAndLastLiElement() {
+  const firstLi = searchLayer.firstElementChild;
+  const lastLi = searchLayer.lastElementChild;
+  return { firstLi, lastLi };
+}
+
 function keyboardNavigationHandler(e) {
+  const { firstLi, lastLi } = getFirstAndLastLiElement();
+  console.log(e.key);
   if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
     e.preventDefault();
   }
   if (e.key === 'ArrowDown') {
     firstLi.focus();
+    searchBar.value = firstLi.innerText;
   }
   if (e.key === 'ArrowUp') {
     lastLi.focus();
+    searchBar.value = lastLi.innerText;
   }
   if (e.keyCode === esc) {
     removeModal(e);
   }
 }
+
 function addFocus(e) {
+  const { firstLi, lastLi } = getFirstAndLastLiElement();
   let onFocus = document.activeElement;
+
   if (e.key !== 'ArrowDown' && e.key !== 'ArrowUp' && e.keyCode !== esc) {
-    console.log('Press Up or Down');
     return;
   } else if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
     e.preventDefault();
@@ -53,19 +70,94 @@ function addFocus(e) {
 
   if (e.key === 'ArrowDown') {
     if (onFocus === lastLi) {
-      onFocus = searchBar.focus();
+      searchBar.focus();
     } else {
-      onFocus = onFocus.nextElementSibling.focus();
+      searchBar.value = onFocus.nextElementSibling.innerText;
+      onFocus.nextElementSibling.focus();
     }
   }
+
   if (e.key === 'ArrowUp') {
     if (onFocus === firstLi) {
-      onFocus = searchBar.focus();
+      searchBar.focus();
     } else {
-      onFocus = onFocus.previousElementSibling.focus();
+      searchBar.value = onFocus.previousElementSibling.innerText;
+      onFocus.previousElementSibling.focus();
     }
   }
   if (e.keyCode === esc) {
     removeModal(e);
   }
+}
+
+///
+
+class JsonData {
+  constructor() {}
+  async load(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('invalid response');
+    }
+    const data = await response.json();
+    // const { recent, suggest } = await response.json();
+    // console.log(recent);
+    // console.log(suggest);
+    return data;
+  }
+}
+
+class ResultView {
+  constructor(parent) {
+    this.parent = parent;
+    console.log(this.parent);
+  }
+
+  renderRecentSearchResults(data) {
+    //초기화 innerHTML = ''
+    //조건
+    //data의 길이가 0이면, 리턴
+    //data의 길이가 0이 아니면 데이터를 가지고 리듀스로 템플릿을 만든다
+    //5개만
+    //insertAdjacentHTML로 템플릿을 부모노드에 붙인걸 리턴 ??
+  }
+  async renderSuggestion(data) {
+    const template = await generateTemplate.suggestion(data);
+    console.log(template);
+    this.parent.insertAdjacentHTML('beforeend', template);
+  }
+}
+
+const generateTemplate = {
+  async suggestion(obj) {
+    const data = await obj;
+    console.log(data.suggest);
+    console.log(data.suggest.length);
+    const suggestion = data.suggest;
+    searchLayer.innerHTML = '';
+
+    if (!suggestion.length) {
+      return;
+    } else {
+      const suggestListTemplate = shuffleArray(suggestion)
+        .slice(0, 10)
+        .reduce((acc, cur) => {
+          return (acc += `<li class="suggestion search-list" tabindex="0">
+          <img src="./src/images/arrow-top-right.svg" alt="이동">
+          <span>${cur}</span>
+        </li>`);
+        }, '');
+      return suggestListTemplate;
+    }
+  },
+  searchResult() {},
+  autocomplete() {},
+};
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
