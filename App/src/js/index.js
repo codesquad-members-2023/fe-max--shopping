048ae7dc2a16e7d4de84c509c3f9d94f2afcf888
saejinpark <saejinpark951210@gmail.com>
2023-04-12 12:35:48 +0900

    clear git cache

--- App/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { App } from "./components/App.js";
-import { viewData } from "./viewData.js";
-(function () {
-  const appInnerData = viewData()
-  const app = new App(appInnerData);
-  app.render();
-  app.onload();
-})();
