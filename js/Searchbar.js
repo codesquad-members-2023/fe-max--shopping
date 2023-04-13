b6fbc21a30879616d55016ab82102d43c325e8a8
silvertae <massenger7@gmail.com>
2023-04-13 18:29:44 +0900

    feat: 검색내역에 json객체에서 받아온 추천검색어 렌더링 기능 구현
    미리 추천검색어를 넣어둔 json 객체를 만들고 여기서부터 데이터를 fetch해서
    받아오는 방식으로 기능을 구현하였다.
    
    기존 Component 클래스는 삭제하고 App.js에서 Searchbar 클래스의 인스턴스를
    만들고 Searchbar의 constructor()에서 SearchbarLayer 클래스의 인스턴스를
    만드는 방식으로 수정하였다.

--- js/Searchbar.js
+++ js/Searchbar.js
@@ -1,6 +1,13 @@
-import { Component } from './Component.js';
+import { SearchbarLayer } from './SearchbarLayer.js';
 
-export class Searchbar extends Component {
+export class Searchbar {
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+    this.layer = new SearchbarLayer(this.$target);
+    this.setEvent();
+  }
+
   template() {
     return `<form class="search-bar__form" action="">
               <input
@@ -17,19 +24,31 @@ export class Searchbar extends Component {
             </form>`;
   }
 
-  addEvent() {
+  render() {
+    this.$target.insertAdjacentHTML('beforeend', this.template());
+  }
+
+  setEvent() {
     const searchbarInput = this.$target.querySelector('.search-bar__input');
     const backdrop = document.querySelector('.modal__backdrop');
 
     searchbarInput.addEventListener('click', () => {
-      const searchbarLayer = this.$target.querySelector('.search-bar__layer');
+      const searchbarLayer = document.querySelector('.search-bar__layer');
       searchbarLayer.classList.add('show');
       backdrop.classList.add('show');
     });
+
     searchbarInput.addEventListener('blur', () => {
-      const searchbarLayer = this.$target.querySelector('.search-bar__layer');
+      const searchbarLayer = document.querySelector('.search-bar__layer');
       searchbarLayer.classList.remove('show');
       backdrop.classList.remove('show');
+    });
+
+    this.$target.addEventListener('keydown', (e) => {
+      // console.log(e.key);
+      if (e.key === 'ArrowDown') {
+        console.log(this.layer);
+      }
     });
   }
 }
