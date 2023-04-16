378f50b38add0b0dc1848c12d7b10f5419f0169b
jay <chlwodnjs34@gmail.com>
2023-04-16 22:04:58 +0900

    feat: main contents 구현

--- src/js/component/main/Main.js
+++ src/js/component/main/Main.js
@@ -1,15 +1,17 @@
 import { Base } from "../Base.js";
 import { Carousel } from "./carousel/Carousel.js";
+import { Contents } from "./contents/Contents.js";
 
 export class Main extends Base {
   constructor() {
     super("div");
     this.carousel = new Carousel();
+    this.contents = new Contents();
     this.init();
   }
 
   init() {
     this.setAttribute("id", "main");
-    this.setChildren(this.carousel);
+    this.setChildren(this.carousel, this.contents);
   }
 }
