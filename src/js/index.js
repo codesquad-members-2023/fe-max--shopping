e2c97ecfec191545e525ca8c9dfab274ceec0b17
JH <vvaa277@gmail.com>
2023-04-18 20:56:49 +0900

    Refactor: json서버 이용하여 히어로 슬라이드 이미지 페칭

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import { initModal } from '../js/events/initModal.js';
 import { initHeroSlide } from '../js/events/initHeroSlide.js';
 import { SearchBar } from '../js/events/search/SearchBar.js';
+import { SideBar } from '../js/events/sideBar/SideBar.js';
 
 const init = async () => {
   initHeroSlide();
   const searchBar = new SearchBar();
   searchBar.initSearchBar();
+  const sideBar = new SideBar();
+  sideBar.initSideBar();
   await initModal();
 };
 init();
