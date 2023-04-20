051c5b61efd3bd58243aa887339cdfd5944a2a8f
hjsong <song6240@naver.com>
2023-04-20 14:16:04 +0900

    style: 사이드바 기본 레이아웃

---
+++ src/js/components/sideBar.js
@@ -0,0 +1,9 @@
+import { $ } from '../utils/domUtils.js'
+
+const sideBarToggle = $('.sidebar__toggle')
+
+sideBarToggle.addEventListener('click', pullSidebarOpen);
+
+function pullSidebarOpen() {
+  
+}
