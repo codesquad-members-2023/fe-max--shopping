3b0222f4f52c147ba81e679b956b613c4ef9dfde
silvertae <massenger7@gmail.com>
2023-04-21 01:02:33 +0900

    feat: 검색바 최근검색어 저장기능 추가
    검색바와 레이어 부분의 코드를 리팩터링 하면서 로컬스토리지를 활용해서
    최근 5개 검색어를 저장해두고 렌더링하는 기능을 추가했다.

--- js/SearchLayer.js
+++ js/SearchLayer.js
@@ -1,74 +1,99 @@
 export class SearchLayer {
   constructor($target) {
     this.$target = $target;
-    this.url = '../data/db.json';
+    this.node = this.$target;
+    this.url = 'http://localhost:5050';
     this.searchDB = {};
     this.setup();
   }
 
   setup() {
-    this.getData()
-      .then((data) => {
-        this.searchDB = data;
-        this.render();
-        this.setEvent();
-      })
-      .catch((error) => console.error(error));
+    this.getData('suggestions').then((data) => {
+      this.searchDB.suggestions = data;
+      this.loadSearchHistory();
+      this.render();
+      this.setEvent();
+    });
   }
 
-  getData() {
-    return fetch(this.url)
-      .then((response) => response.json())
-      .catch((error) => console.error(error));
+  getData(path) {
+    return fetch(`${this.url}/${path}`).then((response) => response.json());
   }
 
-  loadAutoData() {
-    fetch(this.url)
-      .then((response) => response.json())
-      .then((response) => this.fillLayer(response.auto))
-      .catch((error) => console.error(error));
+  loadSearchHistory() {
+    if (localStorage.getItem('searchHistory') === null) {
+      localStorage.setItem('searchHistory', JSON.stringify([]));
+    }
+    this.searchDB.history = JSON.parse(localStorage.getItem('searchHistory'));
   }
 
-  fillLayer(autoArray) {
-    const resultList = document.querySelector('.search-bar__result-container');
-    const autoTemplate = `${autoArray
-      .map(
-        (el, index) =>
-          `<li class="search-bar__result" data-index="${index}">
-        <a href="">${el}</a>
-      </li>`,
-      )
-      .join('')}`;
-    resultList.innerHTML = autoTemplate;
-  }
+  // loadAutoData() {
+  //   const url = `${this.url}/auto`;
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((response) => this.fillLayer(response));
+  // }
+
+  // fillLayer(autoArray) {
+  //   const resultList = document.querySelector('.search-bar__result-container');
+  //   const autoTemplate = `${autoArray
+  //     .map(
+  //       (el, index) =>
+  //         `<li class="search-bar__result" data-index="${index}">
+  //            <a href="">${el}</a>
+  //          </li>`,
+  //     )
+  //     .join('')}`;
+  //   resultList.innerHTML = autoTemplate;
+  // }
 
   template() {
-    const { suggestions } = this.searchDB;
-    return `<div class="search-bar__layer font-BodyMD text-black bg-white">
-              <ul class="search-bar__result-container">
-                 ${suggestions
-                   .map(
-                     (el, index) =>
-                       `<li class="search-bar__result" data-index="${index}">
-                         <img src="./assets/icons/arrow-top-right.svg" alt="" />
-                         <a href="">${el}</a>
-                       </li>`,
-                   )
-                   .join('')}
-              </ul>
-            </div>`;
+    const { suggestions, history } = this.searchDB;
+
+    return `<ul class="search-bar__result-container">
+              ${history
+                .map(
+                  (el, index) =>
+                    `<li class="search-bar__result history" data-index="${index}">
+                      <a href="">${el}</a>
+                      <button>
+                        <img src="./assets/icons/close.svg" alt="" />
+                      </button>  
+                    </li>`,
+                )
+                .join('')} 
+              ${suggestions
+                .map(
+                  (el, index) =>
+                    `<li class="search-bar__result suggestion" data-index="${index}">
+                      <img src="./assets/icons/arrow-top-right.svg" alt="" />
+                      <a href="">${el}</a>
+                    </li>`,
+                )
+                .join('')}
+            </ul>`;
   }
 
   render() {
+    this.$target.innerHTML = '';
     this.$target.insertAdjacentHTML('beforeend', this.template());
   }
 
   setEvent() {
-    const searchbarInput = document.querySelector('.search-bar__input');
+    const $searchForm = document.querySelector('.search-bar__form');
+    const $searchbarInput = document.querySelector('.search-bar__input');
 
-    searchbarInput.addEventListener('keyup', () => {
-      console.log(searchbarInput.value);
-      this.loadAutoData();
+    $searchForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const { history } = this.searchDB;
+      const recentSearchWord = $searchbarInput.value;
+      history.unshift(recentSearchWord);
+      this.searchDB.history = history.slice(0, 5);
+
+      localStorage.setItem('searchHistory', JSON.stringify(this.searchDB.history));
+
+      this.render();
     });
   }
 }
