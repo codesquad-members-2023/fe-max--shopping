7da699a5a80a8034bfe2bbfe6bbcd346b594dc10
wonkyung <mwk0725@gmail.com>
2023-04-21 22:56:09 +0900

    feat: 사이드바 렌더링 클래스 추가

---
+++ src/js/sidebar/renderer.js
@@ -0,0 +1,28 @@
+export class Renderer {
+  constructor(parent, items) {
+    this.items = items;
+    this.parent = parent;
+  }
+
+  renderMainList(location, items) {
+    location.innerHTML = items
+      .map(
+        (item) => `
+      <li class="items">
+        <a href="#">${item}</a>
+        <img src="./src/assets/images/icn_chevron_right.svg" alt="" />
+      </li>
+    `,
+      )
+      .join('');
+  }
+
+  renderSubList(title, items, location) {
+    location.innerHTML = `
+    <div class="title">${title}</div>
+    <ul class="lists">
+      ${items.map((item) => `<li class="items"><a href="#">${item}</a></li>`).join('')}
+    </ul>
+  `;
+  }
+}
