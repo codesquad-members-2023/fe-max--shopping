1fc2ed19f3e97c5eec18acfec95d86bf71d29030
silvertae <massenger7@gmail.com>
2023-04-21 13:29:40 +0900

    feat: 사이드바 html 태그 변경 및 모두 보기 확장기능 추가
    사이드바의 태그를 기존 div 태그에서 ul, li 태그들로 변경했다.
    분류별 쇼핑의 모두 보기 버튼 클릭시 숨어있던 세부 메뉴들이 펼쳐지는
    기능을 추가했다.

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,20 +2,30 @@ export const initSideBar = () => {
   const sideBarLayer = document.querySelector('.side-bar');
   const sidebarOpenBtn = document.querySelector('.nav-sub__sidebar');
   const sidebarCloseBtn = document.querySelector('.side-bar__close-btn');
-  const sideBarLayerContainer = document.querySelector('.side-bar__detail-container');
   const sideBarBackdrop = document.querySelector('.sidebar__backdrop');
+  const hmenuCompressSection = document.querySelector('.hmenu__compress-section');
+  const hmenuExpandBtn = document.querySelector('.hmenu__expand-btn');
+  const hmenuCompressBtn = document.querySelector('.hmenu__compress-btn');
 
   sidebarOpenBtn.addEventListener('click', () => {
     sideBarLayer.style.transform = 'translate(0px)';
-    sideBarLayerContainer.classList.add('show');
     sidebarCloseBtn.style.opacity = 1;
     sideBarBackdrop.classList.add('show');
   });
 
   sidebarCloseBtn.addEventListener('click', () => {
     sideBarLayer.style.transform = 'translate(-332px)';
-    sideBarLayerContainer.classList.remove('show');
     sidebarCloseBtn.style.opacity = 0;
     sideBarBackdrop.classList.remove('show');
+  });
+
+  hmenuExpandBtn.addEventListener('click', () => {
+    hmenuCompressSection.classList.remove('compressed');
+    hmenuCompressBtn.style.visibility = 'visible';
+  });
+
+  hmenuCompressBtn.addEventListener('click', () => {
+    hmenuCompressSection.classList.add('compressed');
+    hmenuCompressBtn.style.visibility = 'hidden';
   });
 };
