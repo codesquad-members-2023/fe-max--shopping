f36bb4b1e80d1aaca85c6f579a12754dd2695775
Jisoo Yoo <youzysu@gmail.com>
2023-04-22 14:05:33 +0900

    refactor: entry point 로직, App 수정
    - after: app node/ before: 통일성을 위해 app node도 만들어 body에 직접 붙여주는 방식
    - body 태그가 하나만 존재하는 건 컨벤션일 뿐, 실제로는 여러개 일 수 있다. (문제 발생 가능)
    - id가 app인 태그(유일성 보장)에 대한 로직

--- src/js/App.js
+++ src/js/App.js
@@ -1,18 +1,19 @@
 import { Navbar } from '../js/components/Navbar/Navbar.js';
 import { Main } from './components/Main.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
-import { Component } from './components/base/Component.js';
 
-export class App extends Component {
-  constructor() {
-    super('app');
+export class App {
+  #node;
+
+  constructor(rootNode) {
+    this.#node = rootNode;
     this.sidebar = new Sidebar();
     this.main = new Main();
     this.header = new Navbar(this.sidebar, this.main);
-    this.init();
+    this.render();
   }
 
-  getTemplate() {
-    return [this.header.node, this.sidebar.node, this.main.node];
+  render() {
+    this.#node.append(this.header.node, this.sidebar.node, this.main.node);
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,4 @@
 import { App } from './App.js';
 
-const body = document.querySelector('body');
-const app = new App();
-
-body.append(app.node);
+const app = document.querySelector('#app');
+new App(app);
