f9d75d4d9033a5b9d9e406ab9372b60b60979371
JH <vvaa277@gmail.com>
2023-04-28 05:57:34 +0900

    Refactor: sideBar mvc적용중

---
+++ src/js/events/sideBarTemp/SideBarModel.js
@@ -0,0 +1,88 @@
+// 사이드바에 필요한 상태
+
+// 내브바에 버튼을 눌렀는지 아닌지
+// 내브바 모두 버튼을 누르면
+// 사이드바 출현
+// 사이드바 닫기 버튼 누르면
+// 사이드바 닫힘
+// 모두보기를 눌렀는지 아닌지
+// 모두보기를 누르면
+// 메뉴 다 보임
+// 메뉴를 클릭했는지 아닌지
+// 메뉴를 누르면// 누른 것의 상태가 변경되면
+// 하위 메뉴가 새로 렌더링됨
+
+export class SideBarModel {
+  constructor() {
+    this.state = {
+      isSideBarOpen: false,
+      isExpanded: false,
+      //   currentMenu,
+    };
+    this.data = {
+      digital: {},
+      shopping: {},
+      collapsible: {},
+    };
+
+    this.data;
+    // this.data;
+    this.subList = [];
+  }
+  setJsonData(obj) {
+    this.data.digital = obj.digital
+    this.data.shopping = obj.shopping
+    this.data.collapsible = obj.collapsible
+    return this.data 
+  }
+
+  getJsonData() {
+    console.log(this.data);
+    return this.data;
+  }
+
+  setSideBarOpenState() {
+    this.state.isSideBarOpen = !this.state.isSideBarOpen;
+  }
+
+  getSideBarOpenState() {
+    return this.state.isSideBarOpen;
+  }
+
+  setExpandedState() {
+    this.state.isExpanded = !this.state.isExpanded;
+  }
+
+  getExpandedState() {
+    return this.state.isExpanded;
+  }
+
+  setCurrentMenu(menuText) {
+    this.state.currentMenu = menuText;
+  }
+  getSubList(menuObj) {
+    return Object.keys(menuObj);
+  }
+
+  async getAll() {
+    return this.users;
+  }
+
+  async getById(id) {
+    return this.users.find(user => user.id === id);
+  }
+
+  async add(user) {
+    this.users.push(user);
+  }
+
+  async update(id, user) {
+    const index = this.users.findIndex(u => u.id === id);
+    this.users[index] = user;
+  }
+
+  async delete(id) {
+    const index = this.users.findIndex(u => u.id === id);
+    this.users.splice(index, 1);
+  }
+}
---
+++ src/js/events/sideBarTemp/SideBarTemp.js
@@ -0,0 +1,154 @@
+import { PATH, URL } from '../../constants/path.js';
+import { handleDimming, layerOpenState } from '../../utils/dim.js';
+import { TemplateGenerator } from '../search/TemplateGenerator.js';
+
+const sideArea = document.querySelector('.side');
+const sideBox = document.querySelector('.side__box');
+const closeBtn = document.querySelector('.side__close-btn');
+const sideViewAll = document.querySelector('.side__view-all');
+const sideListDigital = document.querySelector('.side__list-digital');
+const sideListShoopping = document.querySelector('.side__list-shopping');
+const sideListMain = document.querySelector('.side__list-main');
+const sideListSub = document.querySelector('.side__list-sub');
+
+export class JSONFetcher {
+  constructor(url) {
+    this.url = url;
+  }
+  getData() {
+    return fetch(`${this.url}/${PATH.side}`).then(response => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    });
+  }
+}
+export class DataConverter {
+  constructor(url) {
+    this.jsonFetcher = new JSONFetcher(url);
+  }
+  getObject(type) {
+    return this.jsonFetcher.getData().then(result => {
+      return result[type];
+    });
+  }
+  //todo: JSONFetcher와 통합하기
+}
+
+export class SideBar {
+  constructor(sideBarModel, sideBarview) {
+    this.model = sideBarModel;
+    this.view = sideBarview;
+    // this.fetchJson().then(result => {
+    //   console.log(result);
+    // });
+    // this.getObject('digital').then(result => {
+    //   console.log(result);
+    // });
+    this.setObj();
+    // this.model.getJsonData();
+  }
+  init() {
+    this.view.toggleSidebar();
+
+    const jsonData = this.model.getJsonData();
+    this.view.renderInitialMenu(jsonData);
+
+    sideViewAll.addEventListener('click', () => {
+      sideListMain.classList.remove('compressed');
+    });
+
+    const sideViewSimple = document.querySelector('.side__view-simple');
+    sideViewSimple.addEventListener('click', () => {
+      sideListMain.classList.add('compressed');
+    });
+  }
+
+  async fetchJson() {
+    const response = await fetch(`${URL.jsonBase}/${PATH.side}`);
+    const fetchedJson = await response.json();
+    return fetchedJson;
+  }
+
+  async setObj() {
+    this.model.setJsonData(await this.fetchJson());
+  }
+
+  getObject(type) {
+    return this.fetchJson().then(result => {
+      return result[type];
+    });
+  }
+
+  // getObject(type) {
+  //   return this.jsonFetcher.getData().then(result => {
+  //     return result[type];
+  //   });
+  // }
+}
+
+export class SideBarView {
+  constructor(parent) {
+    this.templateGenerator = new TemplateGenerator();
+  }
+
+  toggleSidebar() {
+    document.addEventListener('click', e => {
+      if (e.target.closest('.hamburger-btn')) {
+        layerOpenState.sidebar = true;
+        sideArea.classList.add('active');
+        closeBtn.classList.remove('close');
+        handleDimming();
+      } else if (!e.target.closest('.side')) {
+        layerOpenState.sidebar = false;
+        sideArea.classList.remove('active');
+        closeBtn.classList.add('close');
+        handleDimming();
+      }
+    });
+  }
+  menuRenderer(parentNode, template) {
+    parentNode.innerHTML = template;
+  }
+
+  renderInitialMenu(jsonData) {
+    const { digital, shopping } = jsonData;
+    console.log(digital);
+    console.log(shopping);
+  }
+
+  expandMenu() {
+    sideListMain.classList.remove('compressed');
+  }
+  compressMenu() {
+    sideListMain.classList.add('compressed');
+  }
+  openSubMenu(node, menuObj) {
+    document.querySelector(node).addEventListener('click', e => {
+      if (e.target.dataset.id) {
+        return;
+      } else {
+        const keyText = e.target.innerText;
+        const subTemplate = this.templateGenerator.generateSubMenu(
+          menuObj,
+          keyText
+        );
+        menuRenderer(sideListSub, subTemplate);
+        sideBox.classList.add('translateX');
+      }
+    });
+  }
+  closeSubMenu() {
+    sideListSub.addEventListener('click', e => {
+      if (!e.target.dataset.id) {
+        return;
+      } else {
+        console.log(e.target.dataset.id);
+        sideBox.classList.remove('translateX');
+      }
+    });
+  }
+}
+
+function menuRenderer(parentNode, template) {
+  parentNode.innerHTML = template;
+}
