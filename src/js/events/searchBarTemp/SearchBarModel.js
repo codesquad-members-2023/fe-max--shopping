da144414e0658f5f2eca5087e67e6f5903398338
JH <vvaa277@gmail.com>
2023-04-28 15:53:49 +0900

    Refactor: 파일 분리 및 주석 제거

--- src/js/events/searchBarTemp/SearchBarModel.js
+++ src/js/events/searchBarTemp/SearchBarModel.js
@@ -1,9 +1,3 @@
-import { $ } from '../../utils/dom.js';
-import { APIClient, JSONClient } from '../api/api.js';
-import { getRandomLetter } from '../../utils/pickPrefix.js';
-import { PATH } from '../../constants/path.js';
-import { SearchHistoryManager } from './SearchHistoryManagerTemp.js';
-import { handleDimming, layerOpenState } from '../../utils/dim.js';
 import { NUMBER } from '../../constants/number.js';
 
 export class SearchBarModel {
