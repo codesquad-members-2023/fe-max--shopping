ac0617a2e4e6eeca46da2828792bc5c984ac3293
Jisoo Yoo <youzysu@gmail.com>
2023-04-28 13:20:29 +0900

    feat: footer 컴포넌트 추가

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 import { Navbar } from '../js/components/Navbar/Navbar.js';
+import Footer from './components/Footer.js';
 import { Main } from './components/Main/Main.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
 
@@ -10,10 +11,11 @@ export class App {
     this.sidebar = new Sidebar();
     this.main = new Main();
     this.header = new Navbar(this.sidebar, this.main);
+    this.footer = new Footer();
     this.render();
   }
 
   render() {
-    this.#node.append(this.header.node, this.sidebar.node, this.main.node);
+    this.#node.append(this.header.node, this.sidebar.node, this.main.node, this.footer.node);
   }
 }
