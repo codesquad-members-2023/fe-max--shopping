23f1485ee69306d884a57838a78715dfffaa902b
qkdflrgs <qkdflrgs@gmail.com>
2023-04-30 20:27:08 +0900

    feat: 배송지역 설정 모달 MVC 패턴 적용

---
+++ src/js/Controller/LocationModalController.js
@@ -0,0 +1,24 @@
+import { $ } from '../Utils.js';
+
+export class LocationModalController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    this.setEvent();
+  }
+
+  async setEvent() {
+    $('.nav-main__location').addEventListener('mouseover', this.open.bind(this));
+    $('.nav-main__location').addEventListener('mouseout', this.close.bind(this));
+  }
+
+  open() {
+    this.model.openLocationModal();
+    this.view.update(this.model.isOpen);
+  }
+
+  close() {
+    this.model.closeLocationModal();
+    this.view.update(this.model.isOpen);
+  }
+}
